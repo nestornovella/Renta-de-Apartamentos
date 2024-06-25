74ac78a2c9d9f05e0b7b7c1bd511849a784801e1
Nestor novella <importarnorth@gmail.com>
2024-06-25 17:47:01 -0300

    change

--- api/src/middleware/rateLimited.js
+++ api/src/middleware/rateLimited.js
@@ -5,7 +5,7 @@ const rateLimiter = require('express-rate-limit')
 const hourLimit100 = rateLimiter(
     {
         windowMs: 60 * 60 * 1000, // 1 hora
-        max: 150, 
+        max: 1000, 
         message: 'Haz excedido el límite de solicitudes permitidas. Por favor, intenta más tarde.'
         
     }
