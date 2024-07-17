e0dbc9fec4889b7ceeb1cf92594afb7014a7a06a
Nestor novella <importarnorth@gmail.com>
2024-07-17 01:27:41 -0300

    chnages

--- clients/src/hooks/admin/updateRentStatus.js
+++ clients/src/hooks/admin/updateRentStatus.js
@@ -7,18 +7,21 @@ function useUpdateRentStatus(reloadTransactions) {
   const [error, setError] = useState(null);
 
   const VITE_API_RENT = import.meta.env.VITE_API_RENT_GENERATE
+  const {alertTop} = useGetAlert()
   
   const updateRentStatus = async (rentId, status) => {
+    console.log("🚀 ~ updateRentStatus ~ rentId:", rentId)
+    console.log("🚀 ~ updateRentStatus ~ status:", status)
     setLoading(true);
     setError(null);
 
-    const {alertTop} = useGetAlert()
     try {
       const response = await axios.put(`${VITE_API_RENT}${rentId}`, status);
       if (!response.ok) {
         throw new Error("Failed to update rent status");
       }
       const data = await response.data;
+      console.log("🚀 ~ updateRentStatus ~ data:", data)
       setLoading(false);
       // Recargar transacciones después de actualizar el estado
       if (reloadTransactions) {
