323eac1d17df4031e073281c0902d36edc4c37de
Nestor novella <importarnorth@gmail.com>
2024-07-19 20:13:03 -0300

    changes

--- clients/src/components/admin/tutorialsSection/TutorialSection.jsx
+++ clients/src/components/admin/tutorialsSection/TutorialSection.jsx
@@ -1,16 +1,20 @@
 
 import ReactPlayer from 'react-player'
 import Transition from '../../complements/transition';
+import MainSection from '../mainSection';
 function TutorialSection() {
-    return ( 
+    return (
         <Transition>
-            <h1 >Tutorial para el uso de aplicacion</h1>
-            <p className='text-sm text-gray-500'>medellin furnished apartments</p>
-            <ReactPlayer url='https://www.youtube.com/watch?v=x3b72cY3o0M'/>
-            <p className='text-sm text-gray-500 font-semibold my-2'>created with ❤️ by Alliance Developers.</p>
-            
+                <div className='w-100%'>
+                    <h1 >Tutorial para el uso de aplicacion</h1>
+                    <p className='text-sm text-gray-500'>medellin furnished apartments</p>
+                    <div className='w-[100%]'>
+                        <ReactPlayer  url='https://www.youtube.com/watch?v=x3b72cY3o0M' />
+                    </div>
+                    <p className='text-sm text-gray-500 font-semibold my-2'>created with ❤️ by Alliance Developers.</p>
+                </div>
         </Transition>
-     );
+    );
 }
 
 export default TutorialSection;
